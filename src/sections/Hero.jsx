d91c4ade59e0e7ae57dc61565512a0fc0348dca7
akash kennedy <akashkennedy1@gmail.com>
2024-04-16 19:19:41 +0530

    Create Hero Section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,24 @@
+import Astronaut from "../assets/home/astronaut.png"
+import flake1 from "../assets/home/flakes-1.png"
+import Button from "../components/Button"
+import flake2 from "../assets/home/flakes-2.png"
+
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="flex items-center justify-between">
+      <div className="flex flex-col justify-between gap-5">
+      <img src={flake1} alt="" className="h-18 w-24 ml-5"/>
+        <h2 className="text-7xl">There is a New Way to Learn More...</h2>
+        <p className="info-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, sequi Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, assumenda saepe! Hic accusantium velit sapiente excepturi .</p>
+        <div className="flex gap-5">
+        <Button label="Sign Up"/>
+        <Button label="Log In"/> 
+        </div>
+        <img src={flake2} alt="" className="h-18 w-24 ml-5"/>
+      </div>
+      <img src={Astronaut} alt="Astronaut"  className="h-[600px]"/>
+    </section>
   )
 }
 
