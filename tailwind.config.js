1a1adea4e83662d1c7943f608a7be708a253c119
akash kennedy <akashkennedy1@gmail.com>
2024-04-16 10:16:36 +0530

    Update Preflight Styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#E5E5E5",
+        black: "00160A",
+        teal: "#64FCD9",
+      },
+    },
   },
   plugins: [],
-}
+};
