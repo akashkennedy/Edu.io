a955e51d2288db39df7a0510f321e270c849fe5f
akash kennedy <akashkennedy1@gmail.com>
2024-04-19 18:41:42 +0530

    Update Navbar

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -2,7 +2,7 @@ import HamburgerMenuIcon from "../assets/hamburgericon.png"
 
 const HamburgerMenu = ({className}) => {
   return (
-    <img src={HamburgerMenuIcon} className="hidden" />
+    <img src={HamburgerMenuIcon} className={`${className}`} />
   )
 }
 
