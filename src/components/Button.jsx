2b03b73dab9e84269452c63cf573ee297131a54f
akash kennedy <akashkennedy1@gmail.com>
2024-04-19 15:07:56 +0530

    Remove Vite Img

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -12,7 +12,7 @@ const Button = ({ label, className }) => {
 
 Button.propTypes = {
   label: PropTypes.string.isRequired,
-  className: PropTypes.string.isRequired,
+  className: PropTypes.string
 };
 
 export default Button;
