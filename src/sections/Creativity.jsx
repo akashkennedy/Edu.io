1965c3dfda6e91fc33c3e439f189f219f3435552
akash kennedy <akashkennedy1@gmail.com>
2024-04-20 17:12:26 +0530

    Update Creativity Section

--- src/sections/Creativity.jsx
+++ src/sections/Creativity.jsx
@@ -5,14 +5,14 @@ import flake2 from "../assets/home/flakes-2.png"
 
 const Creativity = () => {
   return (
-    <section className="flex justify-between items-center gap-10 px-10">
-      <img src={Artist} alt="Artist" className="w-full"/>
+    <section className="flex flex-col px-5 mt-5 sm:justify-between items-center gap-10 sm:px-10">
+      <img src={Artist} alt="Artist" className="size-86 sm:w-full"/>
       <div className="flex flex-col justify-between gap-10">
-        <img src={flake1} alt="" className="h-18 w-24 mr-5" />
-        <h2 className="text-7xl">We Know How to Build Your Creativity...</h2>
-        <p className="info-text w-4/5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus asperiores accusantium voluptatibus a adipisci veritatis, deserunt architecto numquam sequi quod quas neque alias, corrupti dolores deleniti commodi nostrum mollitia totam!</p>
-        <Button label="Get Started!" className="w-60"/>
-        <img src={flake2} alt="" className="h-18 w-24 ml-5 rotate-12 relative bottom-5 left-96"/>
+        <img src={flake1} alt="" className="hidden sm:h-18 w-24 mr-5" />
+        <h2 className=" text-4xl sm:text-7xl">We Know How to Build Your Creativity...</h2>
+        <p className="info-text w-full sm:w-4/5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus asperiores accusantium voluptatibus a adipisci veritatis, deserunt architecto numquam sequi quod quas neque alias, corrupti dolores deleniti commodi nostrum mollitia totam!</p>
+        <Button label="Get Started!" className="w-full sm:w-60"/>
+        <img src={flake2} alt="" className="hidden sm:h-18 w-24 ml-5 rotate-12 relative bottom-5 left-96"/>
       </div>
     </section>
   )
