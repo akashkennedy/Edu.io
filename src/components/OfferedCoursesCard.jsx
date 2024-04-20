8566773bf9c510cac911f3aa4718cded854e442c
akash kennedy <akashkennedy1@gmail.com>
2024-04-20 17:35:45 +0530

    Update Offered Courses Section

--- src/components/OfferedCoursesCard.jsx
+++ src/components/OfferedCoursesCard.jsx
@@ -3,10 +3,10 @@ import Button from "../components/Button";
 
 const OfferedCoursesCard = ({ label, src }) => {
   return (
-    <div className="flex flex-col items-center justify-between h-96 w-72 border-2 border-black rounded-md card-shadow py-3">
-      <img src={src} alt={label} className="h-60" />
+    <div className="w-84 h-[500px] flex items-center justify-between flex-col sm:h-96 sm:w-72 border-2 border-black rounded-md card-shadow py-3">
+      <img src={src} alt={label} className="h-full sm:h-60" />
       <h2 className="text-3xl">{label}</h2>
-      <Button label="Learn More" />
+      <Button label="Learn More" className="mt-5"/>
     </div>
   );
 };
--- src/sections/Creativity.jsx
+++ src/sections/Creativity.jsx
@@ -5,14 +5,14 @@ import flake2 from "../assets/home/flakes-2.png"
 
 const Creativity = () => {
   return (
-    <section className="flex flex-col px-5 mt-5 sm:justify-between items-center gap-10 sm:px-10">
+    <section className="flex flex-col px-5 mt-5 sm:justify-between sm:flex-row items-center gap-10 sm:px-10">
       <img src={Artist} alt="Artist" className="size-86 sm:w-full"/>
       <div className="flex flex-col justify-between gap-10">
-        <img src={flake1} alt="" className="hidden sm:h-18 w-24 mr-5" />
+        <img src={flake1} alt="" className="hidden sm:block h-18 w-24 mr-5" />
         <h2 className=" text-4xl sm:text-7xl">We Know How to Build Your Creativity...</h2>
         <p className="info-text w-full sm:w-4/5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus asperiores accusantium voluptatibus a adipisci veritatis, deserunt architecto numquam sequi quod quas neque alias, corrupti dolores deleniti commodi nostrum mollitia totam!</p>
         <Button label="Get Started!" className="w-full sm:w-60"/>
-        <img src={flake2} alt="" className="hidden sm:h-18 w-24 ml-5 rotate-12 relative bottom-5 left-96"/>
+        <img src={flake2} alt="" className="hidden sm:block h-18 w-24 ml-5 rotate-12 relative bottom-5 left-96"/>
       </div>
     </section>
   )
