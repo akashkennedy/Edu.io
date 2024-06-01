dd7b9626b722cd537e197a640daad8b5e92dbf57
Akash Kennedy <akashkennedy1@gmail.com>
2024-06-01 15:17:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,8 @@ import SignIn from "./pages/SignIn.jsx"
 import ContactUs from "./pages/ContactUs.jsx"
 import Home from "./pages/Home.jsx"
 import NotFound from "./pages/NotFound.jsx";
-import { useLocation } from "react";
-
 
 const App = () => {
-  const location = useLocation();
-  const getLinkClass = (path) => (location.pathname === path ? 'active' : '');
-  
   return (
   <main className="h-screen">
     <Navbar />
